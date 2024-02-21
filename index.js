cb5ec257adf9bd61f72eb25a7aad1a1de911773a
nojux-official <nojus.sajauskas@gmail.com>
2024-02-21 14:12:17 +0200

    Api route implemented

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/api", (req, res) => {
+  const status = {
+     "status": "success"
+  };
+  
+  res.send(status);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
